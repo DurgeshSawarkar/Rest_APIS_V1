93fa6356ec16626b89e574604d84d4bfc7025d43
Durgesh Anil Sawarkar <139369179+DurgeshSawarkar@users.noreply.github.com>
2025-10-01 15:05:06 +0530

    Default fillter

--- APIS/views.py
+++ APIS/views.py
@@ -181,6 +181,7 @@ class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     pagination_class = CustomPagination
+    filterset_fields = ['post']
 
 
 
