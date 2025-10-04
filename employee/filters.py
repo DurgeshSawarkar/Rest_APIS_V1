bc5ec29f9c8f71acebd4d0908f3195bf6816c05f
Durgesh Anil Sawarkar <139369179+DurgeshSawarkar@users.noreply.github.com>
2025-10-04 08:50:57 +0530

    custom filter by emp id

--- employee/filters.py
+++ employee/filters.py
@@ -4,8 +4,17 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
     post = django_filters.CharFilter(field_name='post', lookup_expr='iexact')
     employee_name = django_filters.CharFilter(field_name='employee_name',lookup_expr='iexact' )
-    id = django_filters.RangeFilter(field_name='id')
+    # id = django_filters.RangeFilter(field_name='id')
+    id_min = django_filters.CharFilter(method='filter_by_id_range', label='From Id')
+    id_max = django_filters.CharFilter(method='filter_by_id_range', label='To Id')
 
     class Meta:
         model = Employee
-        fields =['post', 'employee_name', 'id']
+        fields =['post', 'employee_name', 'id_min', 'id_max']
+
+    def filter_by_id_range(self, queryset, name,value):
+        if name == 'id_min':
+            return queryset.filter(employee_id__gte=value)
+        elif name == 'id_max':
+             return queryset.filter(employee_id__lte=value)
+        return queryset
